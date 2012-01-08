7d181f9eb1d12232f4f899ad1a22cf75d81b3d39
heruan <heruan@aldu.net>
2012-01-08 18:22:44 +0100

    Added: aldu.ui.js

---
+++ aldu/latest/aldu.ui.js
@@ -0,0 +1,49 @@
+Aldu.UI = {
+  Panel : {
+    init : function() {
+      $(document).ready(function() {
+        $('.aldu-helpers-panel').children().each(function(i, panel) {
+          var status = $(panel).data('status');
+          var height = $(panel).outerHeight(true);
+          switch (status) {
+          case 'off':
+            $(panel).css('top', "-" + height + "px");
+            $(panel).parent().css({
+              'height' : "0px"
+            });
+            break;
+          case 'on':
+          default:
+            $(panel).css('top', "0px");
+            $(panel).parent().css({
+              'height' : height + "px"
+            });
+          }
+          $('a.toggle', panel).click(function() {
+            switch (status) {
+            case 'off':
+              $(panel).animate({
+                top : '+=' + height
+              });
+              $(panel).parent().animate({
+                height : '+=' + height
+              });
+              status = 'on';
+              break;
+            case 'on':
+              $(panel).parent().animate({
+                height : '-=' + height
+              });
+              $(panel).animate({
+                top : '-=' + height
+              });
+              status = 'off';
+              break;
+            }
+            $(panel).data('status', status);
+          });
+        });
+      });
+    }
+  },
+};
