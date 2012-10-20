e7338b8996c981d93e7640a4e0f38c49203ff9d9
Giovanni Lovato <heruan@aldu.net>
2012-10-20 12:50:48 +0200

    Updated aldu.ui.forms.css

--- aldu/latest/aldu.ui.js
+++ aldu/latest/aldu.ui.js
@@ -337,7 +337,7 @@ Aldu.extend({
             Aldu.log('Aldu.UI.Form.init', 2);
             if (!form.elements.length) {
               var container = $(form).parent();
-              $('div.aldu-helpers-html-form', container).remove();
+              $('div.aldu-core-view-helper-html-form', container).remove();
               $(form).siblings().appendTo(form);
             }
             $.tools.validator.fn('[data-equals]', Aldu
@@ -435,7 +435,7 @@ Aldu.extend({
                   $(fieldset).addClass('collapsed');
                   div.hide();
                 }
-                $('.aldu-helpers-html-form-element', legend).click(function(e) {
+                $('.aldu-core-view-helper-html-form-element', legend).click(function(e) {
                   e.stopPropagation();
                 });
                 $(':checkbox, :radio', legend).change(
@@ -487,37 +487,159 @@ Aldu.extend({
           });
       },
       combobox : function() {
+        if (!$.ui.combosearch) {
+          $.widget("ui.combosearch", {
+            _create : function() {
+              var self = this;
+              var orig = this.element.hide();
+              var value = orig.val();
+              var wrapper = this.wrapper = $("<span>").addClass("ui-combobox").insertAfter(orig);
+              var required = this.element.prop('required');
+              this.element.prop('required', false);
+              var autofocus = this.element.prop('autofocus');
+              var datalist = $('#' + this.element.data('list'));
+              var fields = [];
+              var inputs = {};
+              var main = null;
+              var others = {};
+              datalist.children('option').each(function(i, option) {
+                var field = $(option).val();
+                fields[i] = $(option).text();
+                inputs[field] = $("<input>").attr({
+                  'placeholder' : fields[i],
+                }).appendTo(wrapper).val(value).addClass('ui-combobox-input ui-widget ui-widget-content');
+                if (i === 0) {
+                  main = inputs[field];
+                }
+                else {
+                  others[field] = inputs[field];
+                }
+              });
+              main.autocomplete({
+                minLength : 0,
+                delay : 0,
+                source : function(request, response) {
+                  var search = {};
+                  $.each(inputs, function(field, input) {
+                    search[field] = {'$regex' : $(input).val()};
+                  });
+                  $.ajax({
+                    'url' : orig.data('source'),
+                    'data' : {
+                      'render' : 'json',
+                      'search' : search
+                    },
+                    'success' : function(data) {
+                      response($.map(data, function(model) {
+                        return {
+                          'value' : model.name,
+                          'model' : model
+                        };
+                      }));
+                    }
+                  });
+                },
+                select : function(event, ui) {
+                  orig.val(ui.item.model.id);
+                  orig.trigger('change');
+                  $.each(inputs, function(field, input) {
+                    $(input).val(ui.item.model[field]);
+                  });
+                }
+              });
+              main.data('autocomplete')._renderItem = function(ul, item) {
+                var li = $("<li></li>").data("item.autocomplete", item).appendTo(ul);
+                $.each(inputs, function(field, input) {
+                  var width = input.width();
+                  $("<a>" + item.model[field] + "</a>").width(width).css('display', 'inline-block').appendTo(li);
+                });
+                return li;
+              };
+              $.each(others, function(field, input) {
+                input.on('keyup.ui.aldu', function(event) {
+                  switch (event.keyCode) {
+                  case 40:
+                    main.focus();
+                    break;
+                  default:
+                    main.autocomplete('search');
+                  }
+                });
+              });
+              $("<a>").attr({
+                tabIndex : -1,
+                title : Aldu.t("Clear")
+              })
+              .appendTo(wrapper).button({
+                icons : {
+                  primary : "ui-icon-cancel"
+                },
+                text : false
+              }).removeClass("ui-corner-all").addClass(
+                "ui-corner-right ui-combobox-toggle").click(function() {
+                  orig.val('');
+                  $.each(inputs, function(field, input) {
+                    $(input).val('');
+                  });
+              });
+            },
+            destroy : function() {
+              this.wrapper.remove();
+              this.element.show();
+              $.Widget.prototype.destroy.call(this);
+            }
+          });
+        }
         if (!$.ui.combobox) {
           $.widget("ui.combobox", {
             _create : function() {
-              var input, self = this, select = this.element.hide();
-              var selected = select.children(":selected"), value = selected
-                .val() ? selected.text() : "", wrapper = this.wrapper = $(
-                "<span>").addClass("ui-combobox").insertAfter(select);
+              var self = this;
+              var select = this.element.hide();
+              var selected = select.children(":selected");
+              var value = selected.val() ? selected.text() : "";
+              var wrapper = this.wrapper = $("<span>").addClass("ui-combobox").insertAfter(select);
               var required = this.element.prop('required');
               this.element.prop('required', false);
-              input = $("<input>").prop('required', required).attr({
-                form : this.element.attr('form')
-              });
-              input.appendTo(wrapper).val(value).addClass("ui-combobox-input")
-                .autocomplete(
+              var autofocus = this.element.prop('autofocus');
+              var input = $("<input>").prop({
+                'required' : required,
+                'autofocus' : autofocus
+              })
+             .appendTo(wrapper)
+             .val(value)
+             .addClass("ui-combobox-input")
+             .autocomplete(
                   {
+                    autoFocus : autofocus,
                     delay : 0,
                     minLength : 0,
                     source : function(request, response) {
-                      // TODO da rivedere
                       if (source = select.data('source')) {
+                        var field = select.data('field');
+                        var search = {};
+                        search[field] = {'$regex' : request.term};
                         $.ajax({
-                          url : source + ':json',
-                          data : {
-                            flat : true,
-                            unique : true,
-                            attributes : [ select.data('name') ],
-                            search : [ [ select.data('name'), 'like',
-                              '%' + request.term + '%' ] ]
+                          'url' : source,
+                          'data' : {
+                            'render' : 'json',
+                            'search' : search,
+                            'options' : {
+                              'limit' : 5
+                            }
                           },
                           success : function(data) {
-                            response(data);
+                            response($.map(data, function(model) {
+                              var option = $('<option>').attr({
+                                value : model.id
+                              }).text(model[field]);
+                              return {
+                                model : model,
+                                value : model[field],
+                                label : model[field],
+                                option : option,
+                                append : true
+                              };
+                            }));
                           }
                         });
                       }
@@ -543,6 +665,9 @@ Aldu.extend({
                       }
                     },
                     select : function(event, ui) {
+                      if (ui.item.append) {
+                        $(select).append(ui.item.option);
+                      }
                       ui.item.option.selected = true;
                       self._trigger("selected", event, {
                         item : ui.item.option
@@ -587,9 +712,25 @@ Aldu.extend({
                   }).addClass("ui-widget ui-widget-content ui-corner-left");
               Aldu.UI.Form.validator(input);
               input.data("autocomplete")._renderItem = function(ul, item) {
-                return $("<li></li>").data("item.autocomplete", item).append(
-                  "<a>" + item.label + "</a>").appendTo(ul);
+                var anchor = $("<a>" + item.label + "</a>");
+                var li = $("<li></li>").data("item.autocomplete", item).append(anchor).appendTo(ul);
+                return li;
               };
+              if (select.data('search')) {
+                $("<a>").attr({
+                  tabIndex : -1,
+                  title : Aldu.t("Search")
+                })
+                .appendTo(wrapper).button({
+                  icons : {
+                    primary : "ui-icon-search"
+                  },
+                  text : false
+                }).removeClass("ui-corner-all").addClass(
+                  "ui-corner-right ui-combobox-toggle").click(function() {
+                });
+              }
+              else {
               $("<a>").attr("tabIndex", -1).attr("title", "Show All Items")
                 .appendTo(wrapper).button({
                   icons : {
@@ -613,6 +754,7 @@ Aldu.extend({
                   input.autocomplete("search", "");
                   input.focus();
                 });
+              }
             },
             destroy : function() {
               this.wrapper.remove();
